0eed43dd43992790abfe8437b6cc0550810863f3
md torikul <mdtorikulislamprogrammer@gmail.com>
2026-01-22 11:49:18 +0600

    solved one problem

---
+++ p1_hero.js
@@ -0,0 +1,15 @@
+
+
+
+function hero(bullets, dragons) {
+   
+  if(dragons * 2 <= bullets){
+    return true
+  } else{
+    return false
+  }
+}
+
+console.log(hero(10, 20));
+ 
+
---
+++ p2.reverseSeq.js
@@ -0,0 +1,8 @@
+const reverseSeq = (n) => {
+  console.log(n);
+  for (let i = 0; i < n; i++) {
+    console.log(n[i]);
+  }
+};
+
+console.log(reverseSeq(5));
